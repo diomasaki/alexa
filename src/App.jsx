7f47ab5e565e13a4582bd2664a4e13bea221dd87
Dio Masaki <sendeross99@gmail.com>
2023-08-17 12:22:57 +0700

    rr

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ProductList from "./pages/ProductList";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 const App = () => {
-  const user = useSelector((state)=> state.user.currentUser)
+  const user = useSelector((state) => state.user.currentUser);
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/signin">
-          {user ? <Redirect to="/"/> : <Login />}
-        </Route>
-        <Route path="/products/:category">
-          <ProductList />
-        </Route>
-      </Switch>
-    </Router>
+    <BrowserRouter basename={"/alexa/"}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/signin">
+            {user ? <Redirect to="/" /> : <Login />}
+          </Route>
+          <Route path="/products/:category">
+            <ProductList />
+          </Route>
+        </Switch>
+      </Router>
+    </BrowserRouter>
   );
 };
 
