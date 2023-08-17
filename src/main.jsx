7e092baa973f4b0a840326eb638bb1994a3f1d8f
Dio Masaki <sendeross99@gmail.com>
2023-08-17 12:31:59 +0700

    wwww

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import { HashRouter } from "react-router-dom/cjs/react-router-dom.min.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <HashRouter>
         <App />
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
